61c64c6f959498067fe085d94cc5cfe8f547ce1f
hellozepp <zhanglin04@daojia-inc.com>
2018-12-26 10:41:59 +0800

    提交数据库文件

---
+++ sqlFile/book.sql
@@ -0,0 +1,125 @@
+-- MySQL dump 10.13  Distrib 5.7.16, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: db_book
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_book`
+--
+
+DROP TABLE IF EXISTS `t_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookName` varchar(20) DEFAULT NULL,
+  `author` varchar(300) DEFAULT NULL COMMENT '作者',
+  `sex` varchar(10) DEFAULT NULL,
+  `price` float DEFAULT NULL COMMENT '''定价''',
+  `bookDesc` varchar(1000) DEFAULT NULL,
+  `bookTypeId` int(11) DEFAULT NULL,
+  `press` varchar(300) DEFAULT NULL COMMENT '''出版社''',
+  `copies` int(11) DEFAULT NULL COMMENT '''册数''',
+  `purchasePrice` float DEFAULT NULL COMMENT '''进价''',
+  `deleted` int(2) DEFAULT '0' COMMENT '''0:未删除,1:删除''',
+  PRIMARY KEY (`id`),
+  KEY `bookTypeId` (`bookTypeId`),
+  CONSTRAINT `t_book_ibfk_1` FOREIGN KEY (`bookTypeId`) REFERENCES `t_booktype` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_booktype`
+--
+
+DROP TABLE IF EXISTS `t_booktype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_booktype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookTypeName` varchar(20) DEFAULT NULL,
+  `bookTypeDesc` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_lease_book`
+--
+
+DROP TABLE IF EXISTS `t_lease_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_lease_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookId` int(11) DEFAULT NULL,
+  `deposit` float DEFAULT NULL COMMENT '押金',
+  `price` float DEFAULT NULL COMMENT '租赁价格',
+  `phoneNumber` int(11) DEFAULT NULL COMMENT '租赁人手机号',
+  `employee` varchar(200) DEFAULT NULL COMMENT '店员',
+  `deleted` int(11) DEFAULT '0' COMMENT '0:not deleted 1:is deleted',
+  `leaseTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '租赁时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `t_lease_book_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='租赁信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_order`
+--
+
+DROP TABLE IF EXISTS `t_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_order` (
+  `id` varchar(30) NOT NULL COMMENT '''订单id''',
+  `total` float DEFAULT NULL COMMENT '''总销售价格''',
+  `royalty` float DEFAULT NULL COMMENT '进价',
+  `price` float DEFAULT NULL COMMENT '单价',
+  `numbers` int(11) DEFAULT NULL COMMENT '销售数量',
+  `employees` varchar(100) DEFAULT NULL COMMENT '店员名称',
+  `update_time` date NOT NULL DEFAULT '0001-01-01' COMMENT '更新时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `t_order_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='销售记录表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_user`
+--
+
+DROP TABLE IF EXISTS `t_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `userType` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-26 10:39:35
